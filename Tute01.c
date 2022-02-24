b02c1aecc68e68659e241b261dae9c888a8f2ae3
it21317712 Jayathilaka <100371675+IT21317712@users.noreply.github.com>
2022-02-24 17:34:39 +0000

    completed 01

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,26 @@
 
 #include <stdio.h>
 
-int main() {
+int main(void) 
+{
+  float mrk1,mrk2,avg=0,tot=0; //variable decleration
   
+  printf("\nInput marks of the first module : ");
+  scanf("%f",&mrk1);
+
+  printf("\nInput marks of the second module : ");
+  scanf("%f",&mrk2);
+
+
+tot=mrk1+mrk2; // to calculate total
+avg=tot/2.0;  // to calculate average
+
+printf("\nAverage : %.2f",avg);
+
+
+
+
+
   return 0;
 }
 
